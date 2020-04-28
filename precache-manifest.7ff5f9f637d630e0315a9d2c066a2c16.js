0c3750115fdedba4b929ac5c67a01446dc988536
subhendu17620 <subhendu17620@gmail.com>
2020-04-28 17:19:51 +0530

    Updates

---
+++ precache-manifest.7ff5f9f637d630e0315a9d2c066a2c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee055f7f3f682481fc01e36bfe19256",
+    "url": "/advice_giver_react/index.html"
+  },
+  {
+    "revision": "175988ac56a894d27c78",
+    "url": "/advice_giver_react/static/css/main.985dabbd.chunk.css"
+  },
+  {
+    "revision": "e7f20214ebf38d3a940d",
+    "url": "/advice_giver_react/static/js/2.8a4e4590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice_giver_react/static/js/2.8a4e4590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175988ac56a894d27c78",
+    "url": "/advice_giver_react/static/js/main.6e7df5b4.chunk.js"
+  },
+  {
+    "revision": "6261834bf85ee1cb4c28",
+    "url": "/advice_giver_react/static/js/runtime-main.9f037340.js"
+  },
+  {
+    "revision": "eaa7f752626b925427d48726a7d1d3b2",
+    "url": "/advice_giver_react/static/media/globe.eaa7f752.jpg"
+  }
+]);
